21065eaa3dc824bc2888f621dfbb5a6bebe73c84
drawholka <mamrokhavlad@gmail.com>
2025-02-26 14:31:34 +0200

    get_name constness fixed. ifndef macros added. refresh_updated_date override for virtual function

--- include/File.hpp
+++ include/File.hpp
@@ -1,15 +1,20 @@
+#ifndef FILE_HPP
+#define FILE_HPP
+
 #include "Node.hpp"
 #include <iostream>
 
 class File: public Node
 {
 
-	void refresh_updated_date();
+	void refresh_updated_date() override;
 public: 
 	File(const std::string n = 0);
 	~File() override = default;
-	const std::string& get_name() override;
+	const std::string& get_name() const override;
 	void print() const override;
 
 	void set_name(const std::string& new_name);
 };
+
+#endif
