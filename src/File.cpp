a5f348e8cd70ab6852e41eaf8933b5f8b7e05c89
drawholka <mamrokhavlad@gmail.com>
2025-02-26 18:35:20 +0200

    print method def tabulation by depth

--- src/File.cpp
+++ src/File.cpp
@@ -1,8 +1,5 @@
 #include "File.hpp"
 
-
-
-
 File::File(const std::string n): Node(FILE_TYPE, n){ 
 	this->created_at = updated_at = std::time(0);
 };
@@ -11,7 +8,6 @@ const std::string& File::get_name() const
 {
 	return name;
 };
-
 
 void File::refresh_updated_date()
 {
@@ -24,12 +20,18 @@ void File::set_name(const std::string& new_name)
 	refresh_updated_date();
 
 };
-void File::print() const
+void File::print(unsigned level) const
 {
 	std::string created(std::ctime(&created_at));
 	std::string updated(std::ctime(&updated_at));
-	
+
 	created.pop_back();
+
+	for(unsigned i = 0; i < level; i++)
+	{
+		std::cout << "   ";
+	};
+	std::cout << " - ";
 
 	std::cout << get_name() << " | " << created << " | " << updated;
 };
