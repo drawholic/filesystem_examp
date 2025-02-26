63a38dda3303bffbdc7765a1c48ed74bd63b65be
drawholka <mamrokhavlad@gmail.com>
2025-02-26 16:06:43 +0200

    get_type method declare

--- include/File.hpp
+++ include/File.hpp
@@ -8,13 +8,19 @@ class File: public Node
 {
 
 	void refresh_updated_date() override;
+
 public: 
+
 	File(const std::string n = 0);
 	~File() override = default;
+
 	const std::string& get_name() const override;
+
 	void print() const override;
 
 	void set_name(const std::string& new_name);
+
+	NodeType get_type() const override;
 };
 
 #endif
