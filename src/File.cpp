a6bffee911fec1b1723811189800744a98233a2a
drawholka <mamrokhavlad@gmail.com>
2025-02-26 14:29:54 +0200

    get_name method updated constness. set_name method added refresh_updated_date call inside. print method updated with newline char

--- src/File.cpp
+++ src/File.cpp
@@ -2,11 +2,12 @@
 
 
 
+
 File::File(const std::string n): Node(FILE_TYPE, n){ 
 	this->created_at = updated_at = std::time(0);
 };
  
-const std::string& File::get_name() 
+const std::string& File::get_name() const
 {
 	return name;
 };
@@ -20,9 +21,10 @@ void File::refresh_updated_date()
 void File::set_name(const std::string& new_name)
 {
 	name = std::move(new_name);
+	refresh_updated_date();
 
 };
 void File::print() const
 {
-	std::cout << "Name: " << name << ", created at: " << std::ctime(&created_at) << ", updated_at: "  << std::ctime(&updated_at) << std::endl;
+	std::cout << "File: " << name << "\ncreated at: " << std::ctime(&created_at) << "updated_at: "  << std::ctime(&updated_at);
 };
